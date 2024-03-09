8618e91a80f471589e58754876d0eb4bf5e6d569
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-03-09 13:35:41 +0800

    Update SNP_filter.sh

--- 2_Population_genetics/SNP_filter.sh
+++ 2_Population_genetics/SNP_filter.sh
@@ -1,1 +1,6 @@
+# 无外类群与含外类群 SNP基础过滤
+vcftools --gzvcf  229_filtered_snp.vcf.gz  --min-alleles 2 --max-alleles 2 --minDP 5  --minGQ 10 --minQ 30 --min-meanDP 6  --max-missing 0.8  --maf 0.05   --recode  --recode-INFO-all  --out 229_filtered_vcftools.vcf.gz
+vcftools --gzvcf  224_filtered_snp.vcf.gz  --min-alleles 2 --max-alleles 2 --minDP 5  --minGQ 10 --minQ 30 --min-meanDP 6  --max-missing 0.8  --maf 0.05   --recode  --recode-INFO-all  --out 224_filtered_vcftools.vcf.gz 
 
+## LD 过滤 生成保留和剔除位点的ID
+plink --vcf 224_filtered_vcftools.vcf.gz.recode.vcf --indep-pairwise 50 5 0.2 --out LD  --allow-extra-chr  --set-missing-var-ids @:#  
