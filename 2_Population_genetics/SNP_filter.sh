abf27bf2d111ff59d2797cb892a4de4af842baae
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-03-10 15:20:00 +0800

    Update SNP_filter.sh

--- 2_Population_genetics/SNP_filter.sh
+++ 2_Population_genetics/SNP_filter.sh
@@ -1,6 +1,7 @@
 # 无外类群与含外类群 SNP基础过滤
 vcftools --gzvcf  229_filtered_snp.vcf.gz  --min-alleles 2 --max-alleles 2 --minDP 5  --minGQ 10 --minQ 30 --min-meanDP 6  --max-missing 0.8  --maf 0.05   --recode  --recode-INFO-all  --out 229_filtered_vcftools.vcf.gz
-
+## After filtering, kept 12315778 out of a possible 183150846 Sites
+## Run Time = 14132.00 seconds
 
 vcftools --gzvcf  224_filtered_snp.vcf.gz  --min-alleles 2 --max-alleles 2 --minDP 5  --minGQ 10 --minQ 30 --min-meanDP 6  --max-missing 0.8  --maf 0.05   --recode  --recode-INFO-all  --out 224_filtered_vcftools.vcf.gz 
 ## After filtering, kept 13215558 out of a possible 172544728 Sites
@@ -8,6 +9,10 @@ vcftools --gzvcf  224_filtered_snp.vcf.gz  --min-alleles 2 --max-alleles 2 --min
 
 
 # LD 过滤 生成保留和剔除位点的ID
+plink --vcf 229_filtered_vcftools.vcf.gz.recode.vcf --indep-pairwise 50 5 0.2 --out LD  --allow-extra-chr  --set-missing-var-ids @:#  
+
+
+
 plink --vcf 224_filtered_vcftools.vcf.gz.recode.vcf --indep-pairwise 50 5 0.2 --out LD  --allow-extra-chr  --set-missing-var-ids @:#  
 ##  wc -l  LD.prune.in
 ## 1822433 LD.prune.in
