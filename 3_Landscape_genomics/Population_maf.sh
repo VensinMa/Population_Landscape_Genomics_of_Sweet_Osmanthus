6ddadfac867c1e3c1311eb497680d4acce62c4ca
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-03-11 17:36:03 +0800

    Update Population_maf.sh

--- 3_Landscape_genomics/Population_maf.sh
+++ 3_Landscape_genomics/Population_maf.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 
-# 输入文件: VCF文件和群体信息文件
+# 输入文件和目录
 vcf_file="224.filtered.LD.pruned.noContig.recode.vcf"
-pop_info_file="224sample.pop" # 第一列为个体ID，第二列为群体名称
+pop_file="224sample.pop"
 
-# 输出结果的文件夹
+# 输出目录
 output_dir="population_maf_results"
+temp_dir="${output_dir}/temp_ids"
 
-# 如果输出文件夹不存在，则创建它
-if [ ! -d "$output_dir" ]; then
-    mkdir -p "$output_dir"
-fi
+# 确保输出和临时目录存在
+mkdir -p "$output_dir"
+mkdir -p "$temp_dir"
 
-# 为每个群体创建个体ID文件
-awk '{print $1 > $2".ids"}' $pop_info_file
+# 从群体信息文件中提取独特的群体，并保存到一个临时文件
+cut -f2 $pop_file | sort | uniq > "${temp_dir}/unique_groups.txt"
 
-# 对每个群体计算次等位基因频率，并在后台执行
-for pop in $(cut -f 2 -d ' ' $pop_info_file | sort | uniq)
-do
-    echo "Processing population: $pop"
-    vcftools --vcf $vcf_file --keep ${pop}.ids --maf --out "${output_dir}/${pop}_maf" &
-done
+# 为每个独特的群体生成包含个体ID的文件，确保每行格式为 "个体ID 个体ID"
+while read group; do
+    grep "\b$group\b" $pop_file | awk '{print $1, $1}' > "${temp_dir}/${group}.ids"
+done < "${temp_dir}/unique_groups.txt"
 
-# 等待所有后台任务完成
+# 使用plink处理每个群体
+while read group; do
+    (
+        echo "处理群体：$group"
+        # 生成BED文件
+        plink --vcf $vcf_file --keep "${temp_dir}/${group}.ids" --make-bed  --allow-extra-chr  --keep-allele-order  --out "${output_dir}/${group}_maf" --set-missing-var-ids @:# 
+        # 确认BED文件已生成
+        if [ $? -eq 0 ]; then
+            # 计算频率
+            plink --bfile "${output_dir}/${group}_maf" --freq --out "${output_dir}/${group}_population_maf"
+        else
+            echo "生成BED文件失败：$group"
+        fi
+        echo "完成群体：$group"
+    ) &
+done < "${temp_dir}/unique_groups.txt"
+
 wait
 
-echo "All populations processed."
-
-
-
+echo "所有群体处理完毕。"
 
