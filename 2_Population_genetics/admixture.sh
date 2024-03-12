54e4807ee8fdeffbe345fb51f5ccec06fa88814d
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-03-12 16:49:11 +0800

    Update admixture.sh

--- 2_Population_genetics/admixture.sh
+++ 2_Population_genetics/admixture.sh
@@ -1,5 +1,5 @@
 # 需要bed格式文件
-plink --vcf 224__filtered.LD.pruned.noContig.recode.vcf --make-bed   --out 224_filtered.LD.pruned.noContig --keep-allele-order --allow-extra-chr  
+plink --vcf 224_filtered.LD.pruned.noContig.recode.vcf --make-bed   --out 224_filtered.LD.pruned.noContig --keep-allele-order --allow-extra-chr  
 
 # 计算K=2到20
 seq 2 20 | parallel -j 20 "admixture --cv  224_filtered.LD.pruned.noContig.bed {} 1>admix.{}.log 2>&1"
