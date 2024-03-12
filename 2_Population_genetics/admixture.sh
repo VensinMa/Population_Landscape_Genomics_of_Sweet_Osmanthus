2d35642eeff8f1d515d12eae83c9c400bee2ad39
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-03-12 16:57:01 +0800

    Update admixture.sh

--- 2_Population_genetics/admixture.sh
+++ 2_Population_genetics/admixture.sh
@@ -1,8 +1,8 @@
 # 需要bed格式文件
 plink --vcf 224_filtered.LD.pruned.noContig.recode.vcf --make-bed   --out 224_filtered.LD.pruned.noContig --keep-allele-order --allow-extra-chr  
 
-# 计算K=2到20
-seq 2 20 | parallel -j 20 "admixture --cv  224_filtered.LD.pruned.noContig.bed {} 1>admix.{}.log 2>&1"
+# 计算K=2到30
+seq 2 30 | parallel -j 20 "admixture --cv  224_filtered.LD.pruned.noContig.bed {} 1>admix.{}.log 2>&1"
 
 # 确定具有最小CV值的K值为最佳分群数
 cat *.log | grep "CV"
