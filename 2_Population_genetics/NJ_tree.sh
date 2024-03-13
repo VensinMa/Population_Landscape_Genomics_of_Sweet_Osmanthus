0945e63eda7ec857a02e8d3663483e256c5b82bc
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-03-13 20:23:37 +0800

    Update NJ_tree.sh

--- 2_Population_genetics/NJ_tree.sh
+++ 2_Population_genetics/NJ_tree.sh
@@ -21,8 +21,20 @@ tar -zxvf v2.8.tar.gz
 
 
 python /root/workspace/pop/vcf2phylip-2.8/vcf2phylip.py --input 224_filtered.LD.pruned.noContig.recode.vcf --fasta --output-prefix 224_filtered.LD.pruned.noContig
+python /root/workspace/pop/vcf2phylip-2.8/vcf2phylip.py --input 229_filtered.LD.pruned.noContig.recode.vcf --fasta --output-prefix 229_filtered.LD.pruned.noContig
+python /root/workspace/pop/vcf2phylip-2.8/vcf2phylip.py --input modi_224.snpEffAnno.4dtv.nocontig.vcf   --fasta --output-prefix 224.snpEffAnno.4dtv.nocontig
+
+/root/workspace/pop/VCF2Dis-1.50/bin/VCF2Dis -i  229_filtered.LD.pruned.noContig.recode.vcf -o 229_filtered.LD.pruned.noContig_dis.mat
 
 treebest nj -b 1000 224_filtered.LD.pruned.noContig.min4.fasta > 224_filtered.LD.pruned.noContig.treebest.out
 treebest nj -W -b 1000 224.filtered.LD.pruned.noContig.min4.fasta > 224.filtered.LD.pruned.noContig.treebest.unroot.tree.out &
+treebest nj -W -b 1000 224.snpEffAnno.4dtv.nocontig.min4.fasta > 224.snpEffAnno.4dtv.nocontig.treebest.unroot.tree.out 
 
 sed 's/:\([0-9.]\+\)\[&&NHX:B=\([0-9]\+\)\]/\2:\1/' 224_filtered.LD.pruned.noContig.treebest.out | awk '{printf $0}' > 224_filtered.LD.pruned.noContig.treebest.nwk
+224.snpEffAnno.4dtv.nocontig.treebest.unroot.tree.out
+sed 's/:\([0-9.]\+\)\[&&NHX:B=\([0-9]\+\)\]/\2:\1/' 224.snpEffAnno.4dtv.nocontig.treebest.unroot.tree.out | awk '{printf $0}' > 224.snpEffAnno.4dtv.nocontig.treebest.unroot.tree.nwk
+
+
+
+
+
