6ddfd987619c05ffd2b264abd122f01c83bc508b
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-03-13 12:02:21 +0800

    Update NJ_tree.sh

--- 2_Population_genetics/NJ_tree.sh
+++ 2_Population_genetics/NJ_tree.sh
@@ -23,4 +23,6 @@ tar -zxvf v2.8.tar.gz
 python /root/workspace/pop/vcf2phylip-2.8/vcf2phylip.py --input 224_filtered.LD.pruned.noContig.recode.vcf --fasta --output-prefix 224_filtered.LD.pruned.noContig
 
 treebest nj -b 1000 224_filtered.LD.pruned.noContig.min4.fasta > 224_filtered.LD.pruned.noContig.treebest.out
+treebest nj -W -b 1000 224.filtered.LD.pruned.noContig.min4.fasta > 224.filtered.LD.pruned.noContig.treebest.unroot.tree.out &
+
 sed 's/:\([0-9.]\+\)\[&&NHX:B=\([0-9]\+\)\]/\2:\1/' 224_filtered.LD.pruned.noContig.treebest.out | awk '{printf $0}' > 224_filtered.LD.pruned.noContig.treebest.nwk
