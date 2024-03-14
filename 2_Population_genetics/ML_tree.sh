b64ce812f82440682a0260da6f892d2964fae67a
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-03-14 19:27:50 +0800

    Update ML_tree.sh

--- 2_Population_genetics/ML_tree.sh
+++ 2_Population_genetics/ML_tree.sh
@@ -1,1 +1,4 @@
 fasttree -nt  229_filtered.LD.pruned.noContig.min4.fasta > 229_filtered.LD.pruned.noContig.fasttree.out
+
+
+iqtree -s 229_filtered.LD.pruned.noContig.min4.fasta  -m MFP  -B 1000  -keep-ident  -T 20 &
