0b1801ae466dfbf38d710e5aae5d8cee0b426464
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-03-15 13:13:34 +0800

    Update 4DTV.sh

--- 2_Population_genetics/4DTV.sh
+++ 2_Population_genetics/4DTV.sh
@@ -6,7 +6,7 @@
 
 ####################################################   PCA  ##################################################
 ## vcf转bed
-plink --vcf 229.snpEffAnno.4dtv.vcf  --make-bed   --out 229_filtered.LD.4dtv.noContig  --keep-allele-order  --allow-extra-chr 
+plink --vcf /public1/guop/mawx/workspace/rxg_wild_snpcalling/pop/229sample_LD_4DTV/229.snpEffAnno.4dtv.vcf  --make-bed   --out 229_filtered.LD.4dtv.noContig  --keep-allele-order  --allow-extra-chr 
 
 ## 生成grm文件
 gcta64 --bfile 229_filtered.LD.4dtv.noContig  --autosome  --make-grm  --out GA
@@ -25,6 +25,26 @@ sed 's/:\([0-9.]\+\)\[&&NHX:B=\([0-9]\+\)\]/\2:\1/' 229_filtered.LD.4dtv.noConti
 ###################################################  ML tree ##################################################
 
 iqtree -s 229_filtered.LD.4dtv.noContig.min4.fasta  -m MFP  -B 1000  -keep-ident  -T 20 &
+
+#################################################  faststructure  ##############################################
+cd /public1/guop/mawx/workspace/rxg_wild_snpcalling/pop/229sample_LD_4DTV/structure/faststructure
+plink --vcf /public1/guop/mawx/workspace/rxg_wild_snpcalling/pop/229sample_LD_4DTV/229.snpEffAnno.4dtv.vcf  --make-bed   --out 229_filtered.LD.4dtv.noContig  --keep-allele-order  --allow-extra-chr  
+mkdir fast_result
+seq 2 30 | parallel -j 30 "structure.py -K {} --input=229_filtered.LD.4dtv.noContig --output=fast_result/229_filtered.LD.4dtv.noContig_faststructure_{} --cv=5 --prior=logistic --seed=123 > faststructure.{}.log 2>&1" &
+
+#################################################  admixture  ##############################################
+cd /public1/guop/mawx/workspace/rxg_wild_snpcalling/pop/229sample_LD_4DTV/structure/admixture
+## 需要bed格式文件 
+plink --vcf /public1/guop/mawx/workspace/rxg_wild_snpcalling/pop/229sample_LD_4DTV/229.snpEffAnno.4dtv.vcf  --make-bed   --out 229_filtered.LD.4dtv.noContig  --keep-allele-order  --allow-extra-chr  
+
+# 计算K=2到30
+seq 2 30 | parallel -j 30 "admixture --cv  229_filtered.LD.4dtv.noContig.bed {} 1>admix.{}.log 2>&1" &
+
+# 确定具有最小CV值的K值为最佳分群数
+cat *.log | grep "CV"
+
+mkdir result
+cp  ./*.Q result/
 
 
 
