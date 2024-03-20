ceec0719aad471172b354d6ef62e7401f74d7662
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-03-20 17:45:33 +0800

    Update SNP_filter.sh

--- RE/SNP_filter.sh
+++ RE/SNP_filter.sh
@@ -15,6 +15,7 @@ bcftools reheader -s rename.id 224_filtered_snp.vcf.gz -o 224_filtered_rename.vc
 
 ###  剔除不确定样本
 vcftools --gzvcf 224_filtered_rename.vcf.gz --recode --recode-INFO-all  --remove-indv remove.id --out 186_raw
+
 #########################################  GATK 粗过滤  ######################################################
 mkdir tmp
 gatk --java-options "-Xmx30g -Xms30g -XX:ParallelGCThreads=16 -Djava.io.tmpdir=./tmp"  VariantFiltration  -R /mnt/e/mwx/workspace/pop/genome/Osmanthus.genomic.fasta -V 186_raw.recode.vcf --filter-expression "QUAL < 30.0 || QD < 2.0 || MQ < 40.0 || FS > 60.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" --filter-name 'gatk_filter' -O 186_filter.vcf.gz
@@ -23,6 +24,7 @@ gatk --java-options "-Xmx30g -Xms30g -XX:ParallelGCThreads=16 -Djava.io.tmpdir=.
 #########################################  vcftools 过滤  #####################################################
 
 vcftools --gzvcf 224_filtered_rename.vcf.gz --remove remove.id --min-alleles 2  --max-alleles 2  --minDP 5  --minGQ 10  --minQ 30 --min-meanDP 6  --max-missing 0.8  --maf 0.05 --recode --recode-INFO-all --out 186_filtered_vcftools
+
 ## After filtering, kept 186 out of 224 Individuals
 ## Outputting VCF file...
 ## After filtering, kept 12557928 out of a possible 172544728 Sites
