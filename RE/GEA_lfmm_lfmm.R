ad1699ac43609ab35dc10733ea37bb591cddb5a4
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-03-21 19:58:41 +0800

    Update GEA_lfmm_lfmm.R

--- RE/GEA_lfmm_lfmm.R
+++ RE/GEA_lfmm_lfmm.R
@@ -9,8 +9,7 @@ library(lfmm)
 library(qvalue)  
 library(data.table)
 
-## 参考教程链接 https://bookdown.org/hhwagner1/LandGenCourse_book/WE_11.html
-# 目标目录
+# 设置工作目录
 getwd()
 dir_lfmm <- "lfmm"
 
@@ -21,15 +20,17 @@ if (!file.exists(dir_lfmm)) {
 }
 setwd(dir_lfmm)
 getwd()
+
 ######################  读入解释变量与响应变量  ###############################
+## X Y 数据中样品顺序（行）要一致
 ## 读取环境数据 （解释变量）
 X = read.csv("186sample_id_lat_lon_ele_with_env_vars.csv", 
              header = T, row.names = 1)
 X = X[,3:25]
-
+str(X)
 ## 读取基因型数据（响应变量）
 Y = fread("186_filtered.LD.pruned.noContig.recode.lfmm")
-K = 5  # admixture确定的最佳分群数
+K = 2  # admixture确定的最佳分群数
 
 mod.lfmm <- lfmm::lfmm_ridge(Y = Y,
                              X = X,
@@ -42,27 +43,155 @@ pv <- lfmm::lfmm_test(Y = Y, X = X,
 # 读取校正后的pv$calibrated.pvalue 作为p值
 pvalues <- pv$calibrated.pvalue
 pv$gif
+# 将 pvalues 矩阵保存为 CSV 文件
+# write.csv(pvalues, file = "calibrated_pvalues.csv", row.names = FALSE)
+fwrite(pvalues, file = "calibrated_pvalues.csv",sep = "," )
 
 ## 将校正后的 p 值进一步校正为 q 值（假发现率，FDR）
-# 使用 lapply 转换 p 值为 q 值，并保留列名
+# pv$calibrated.pvalue 是包含校正后 p 值的矩阵
 qvalues_list <- lapply(1:ncol(pv$calibrated.pvalue), function(i) {
-  col_name <- colnames(pv$calibrated.pvalue)[i] # 获取当前列名
-  q_vals <- qvalue(pv$calibrated.pvalue[, i])$qvalues # 计算q值
-  setNames(list(q_vals), col_name) # 将q值列表赋予列名
+  q_vals <- qvalue(pv$calibrated.pvalue[, i])$qvalues
+  return(q_vals)
 })
-# 将列表转换为矩阵
+colnames(pv$calibrated.pvalue)
+# 为整个列表设置名称
+names(qvalues_list) <- colnames(pv$calibrated.pvalue)
+
+# 转换列表为矩阵
 qvalues_matrix <- do.call(cbind, qvalues_list)
-# 由于列表的每个元素名已经是原始列名，我们可以直接将这些名字设置为矩阵的列名
+
+# 设置矩阵列名
 colnames(qvalues_matrix) <- names(qvalues_list)
+colnames(qvalues_matrix)
+
+# 初始化一个列表来保存每列 q 值小于 0.05 的位点索引
+significant_indices_list <- list()
+
+# 遍历 qvalues_matrix 的每一列
+for(i in 1:ncol(qvalues_matrix)) {
+  # 找到每列中 q 值小于 0.05 的位点的索引
+  significant_indices <- which(qvalues_matrix[, i] < 0.05)
+  # 将找到的索引保存到列表中
+  significant_indices_list[[colnames(qvalues_matrix)[i]]] <- significant_indices
+}
+
+# significant_indices_list 现在包含了每列中显著位点的索引
+
+# 计算每列中显著位点的数量
+significant_counts <- sapply(significant_indices_list, length)
+significant_counts
+# significant_counts 是一个向量，包含每列显著位点的数量
+'''
+    > significant_counts
+         BIO1      BIO2      BIO3      BIO4      BIO5      BIO6      BIO7      BIO8 
+         1357      1075      9271      1162      2767        93       462       896 
+         BIO9     BIO10     BIO11     BIO12     BIO13     BIO14     BIO15     BIO16 
+         2312       565        66      2688      1234       525       287       298 
+        BIO17     BIO18     BIO19 Elevation      SRAD       SOC     PHH2O 
+         1331      7029      9142      2186       864       932      4475 
+'''
 
 
-length(which(qv < 0.10)) ## h.w many SNPs have an FDR < 10%?
-length(which(qv < 0.05)) ## h.w many SNPs have an FDR < 5%?
-(FDR.1 <- colnames(Y)[which(qv < 0.1)]) ## i.entify which SNPs these are
-(FDR.05 <- colnames(Y)[which(qv < 0.05)])
 
-write.csv(FDR.1, "FDR_0.1_SNPs.csv", quote = FALSE, row.names = FALSE)
-write.csv(FDR.05, "FDR_0.05_SNPs.csv", quote = FALSE, row.names = FALSE)
+#####  写出位点名称
+# 指定文件夹名称
+significant_indices_folder_name <- "significant_indices"
+# 检查文件夹是否存在，如果不存在则创建
+if (!dir.exists(significant_indices_folder_name)) {
+  dir.create(significant_indices_folder_name)
+}
+
+# 遍历 significant_indices_list 中的每一列
+for (col_name in names(significant_indices_list)) {
+  # 生成文件名，包含文件夹路径和列名
+  filename <- file.path(significant_indices_folder_name, paste0(col_name, "_significant_indices.txt"))
+  
+  # 获取当前列的显著位点索引
+  significant_indices <- significant_indices_list[[col_name]]
+  
+  # 将索引保存到文件中
+  writeLines(as.character(significant_indices), filename)
+}
+
+# 求显著位点索引的并集
+all_significant_indices <- unique(unlist(significant_indices_list))
+length(all_significant_indices)
+'''
+    > significant_counts
+         BIO1      BIO2      BIO3      BIO4      BIO5      BIO6      BIO7      BIO8 
+         1357      1075      9271      1162      2767        93       462       896 
+         BIO9     BIO10     BIO11     BIO12     BIO13     BIO14     BIO15     BIO16 
+         2312       565        66      2688      1234       525       287       298 
+        BIO17     BIO18     BIO19 Elevation      SRAD       SOC     PHH2O 
+         1331      7029      9142      2186       864       932      4475 
+'''
+
+full_path <- file.path(significant_indices_folder_name, "all_significant_indices.txt")
+
+# 将并集保存到文件中
+writeLines(as.character(all_significant_indices), full_path)
+
+#########################  FDR 0.05 的 p 值阈值  ##############################
+
+# 初始化一个向量来保存每列的 p 值阈值
+pvalue_thresholds <- numeric(ncol(qvalues_matrix))
+
+# 遍历每列
+for(i in 1:ncol(qvalues_matrix)) {
+  # 获取当前列
+  qvalues <- qvalues_matrix[, i]
+  # 找出 q 值 <= 0.05 的最大 q 值
+  max_qvalue_under_threshold <- max(qvalues[qvalues <= 0.05])
+  
+  # 找到与这个 q 值相对应的最小 p 值
+  # 注意：这里假设 p 值与 q 值是单调关联的，这在大多数情况下是成立的
+  corresponding_pvalue <- min(pv$calibrated.pvalue[qvalues == max_qvalue_under_threshold, i])
+  
+  # 保存这个 p 值到阈值向量中
+  pvalue_thresholds[i] <- corresponding_pvalue
+}
+
+pvalue_thresholds
+# pvalue_thresholds 向量现在包含了每列在 FDR 0.05 下的 p 值阈值
+'''
+    > pvalue_thresholds
+     [1] 3.771611e-05 2.990105e-05 2.589362e-04 3.234397e-05 7.727829e-05 2.540473e-06
+     [7] 1.288894e-05 2.494263e-05 6.551284e-05 1.574773e-05 1.729086e-06 7.503425e-05
+    [13] 3.428627e-05 1.461574e-05 7.694276e-06 8.298022e-06 3.715242e-05 1.962802e-04
+    [19] 2.553796e-04 6.080596e-05 2.411788e-05 2.594347e-05 1.249710e-04
+    > 
+'''
+
+
+# 初始化向量来保存每列的 q 值阈值
+qvalue_thresholds <- numeric(ncol(qvalues_matrix))
+
+# 遍历每列，这个循环现在也计算和保存 q 值阈值
+for(i in 1:ncol(qvalues_matrix)) {
+  qvalues <- qvalues_matrix[, i]
+  # 找出 q 值 <= 0.05 的最大 q 值，作为 q 值阈值
+  max_qvalue_under_threshold <- max(qvalues[qvalues <= 0.05])
+  qvalue_thresholds[i] <- max_qvalue_under_threshold  # 保存 q 值阈值
+  
+  # 计算对应的 p 值阈值
+  corresponding_pvalue <- min(pv$calibrated.pvalue[qvalues == max_qvalue_under_threshold, i])
+  pvalue_thresholds[i] <- corresponding_pvalue
+}
+
+# 创建一个数据框来保存 p 值和 q 值阈值
+thresholds_df <- data.frame(qvalue_thresholds = qvalue_thresholds,
+                            pvalue_thresholds = pvalue_thresholds)
+
+# 将阈值数据框写入 CSV 文件
+write.csv(thresholds_df, file = "thresholds.csv", row.names = FALSE)
+
+
+
+
+length(which(qvalues_matrix[,2] < 0.10)) 
+length(which(qvalues_matrix[,2] < 0.05)) 
+
+FDR.05 <- which(qvalues_matrix[,2] < 0.05)
 
 
 ################################  绘图  #####################################
@@ -105,12 +234,29 @@ for (i in 1:ncol(pv$pvalue)) {
 
 
 ##  2、曼哈顿图
+
+plot(-log10(pv$calibrated.pvalue[,1]), 
+     pch = 19, 
+     cex = .3,
+     xlab = "Probe", 
+     ylab = "-Log P",
+     col = "grey",
+     main = "Column 1")
+
+# 在图中添加对应 FDR 0.05 的阈值横线
+threshold_line <- -log10(pvalue_thresholds[1])
+abline(h = threshold_line, col = "red", lwd = 2)
+
+
+
+
+
 plot(-log10(pv$calibrated.pvalue[,18]), 
      pch = 19, 
      cex = .3,
      xlab = "Probe", ylab = "-Log P",
      col = "grey")
-causal.set <- seq(11, 1496, by = 80)
+
 points(causal.set, 
        -log10(pv$calibrated.pvalue)[causal.set], 
        col = "blue")
