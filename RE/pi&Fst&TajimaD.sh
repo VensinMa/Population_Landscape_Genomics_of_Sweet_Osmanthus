2c293ccbff61638e82abebbfcac73b9d6cd04ff9
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-03-22 16:55:21 +0800

    Update pi&Fst&TajimaD.sh

--- RE/pi&Fst&TajimaD.sh
+++ RE/pi&Fst&TajimaD.sh
@@ -6,7 +6,7 @@ vcftools --vcf ../186_filtered.LD.pruned.noContig.recode.vcf --window-pi 100000 
 ##  Outputting Windowed Nucleotide Diversity Statistics...
 ##  After filtering, kept 1789805 out of a possible 1789805 Sites
 ##  Run Time = 20.00 seconds
-
+vcftools --vcf ../186_filtered.LD.pruned.noContig.recode.vcf --window-pi 100000 --out LD.pruned.noContig_pi_100kb_nostep
 
 # 计算Tajima's D值
 vcftools --vcf ../186_filtered.LD.pruned.noContig.recode.vcf --TajimaD 100000 --out LD.pruned.noContig_tajimasD_100kb
@@ -15,17 +15,9 @@ vcftools --vcf ../186_filtered.LD.pruned.noContig.recode.vcf --TajimaD 100000 --
 ##  After filtering, kept 1789805 out of a possible 1789805 Sites
 ##  Run Time = 18.00 seconds
 
-
-
-vcftools --vcf ../186.snpEffAnno.4dtv.LD.vcf --window-pi 100000 --out LD.pruned.4DTV.noContig_pi_100kb
-
-
-
-
-vcftools --vcf ../186.snpEffAnno.4dtv.LD.vcf --TajimaD 100000 --out LD.pruned.noContig_tajimasD_100kb
-
-
-
+vcftools --vcf ../186_filtered.LD.pruned.noContig.recode.vcf --window-pi 100000  --window-pi-step 1000 --out LD.pruned.noContig_pi_100kb
+awk '{$4=""; print $0}' LD.pruned.noContig_pi_100kb_nostep.windowed.pi >  LD.pruned.noContig_pi_100kb_nostep.windowed.pi.txt
+awk '{$4=""; print $0}' LD.pruned.noContig_pi_100kb_nostep.windowed.pi >  LD.pruned.noContig_pi_100kb_nostep.windowed.pi.txt
 
 
 
