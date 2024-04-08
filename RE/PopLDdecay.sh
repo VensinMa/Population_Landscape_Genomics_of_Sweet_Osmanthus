91d58393394c4d32e07f747bf7ad3b9756a174fe
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-04-08 16:21:12 +0800

    Update PopLDdecay.sh

--- RE/PopLDdecay.sh
+++ RE/PopLDdecay.sh
@@ -1,1 +1,13 @@
-PopLDdecay -InVCF Final_snps.vcf -OutStat Out.LDdecay -SubPop pop.list
+
+
+PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop east.pop -OutStat east.PopLDdecay
+PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop west.pop -OutStat west.PopLDdecay
+
+Plot_OnePop.pl -inFile east.PopLDdecay.stat.gz --output east.PopLDdecay.stat -bin1 10 -bin2 100
+Plot_OnePop.pl -inFile west.PopLDdecay.stat.gz --output west.PopLDdecay.stat -bin1 10 -bin2 100
+
+vi multi.list
+east.PopLDdecay.stat.gz east
+west.PopLDdecay.stat.gz west
+
+Plot_MultiPop.pl -inList multi.list --output east_west.PopLDdecay -bin1 10 -bin2 100
