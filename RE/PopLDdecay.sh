32ff113ac9f8ed4a14f329ede3f07165666a3027
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-04-09 19:08:38 +0800

    Update PopLDdecay.sh

--- RE/PopLDdecay.sh
+++ RE/PopLDdecay.sh
@@ -1,13 +1,24 @@
+cd  /root/workspace/186sample/PopLDdecay
+
+PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop East.pop -OutStat East.PopLDdecay &
+PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop West.pop -OutStat West.PopLDdecay &
+PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop Central.pop -OutStat Central.PopLDdecay &
+PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop Central-East.pop -OutStat Central-East.PopLDdecay &
+
+Plot_OnePop.pl -inFile East.PopLDdecay.stat.gz --output East.PopLDdecay.stat -bin1 10 -bin2 100
+Plot_OnePop.pl -inFile West.PopLDdecay.stat.gz --output West.PopLDdecay.stat -bin1 10 -bin2 100
+Plot_OnePop.pl -inFile Central.PopLDdecay.stat.gz --output Central.PopLDdecay.stat -bin1 10 -bin2 100
+
+vi K2_multi.list
+West.PopLDdecay.stat.gz West
+Central-East.PopLDdecay.stat.gz Central-East
+
+vi K3_multi.list
+West.PopLDdecay.stat.gz West
+Central.PopLDdecay.stat.gz Central
+East.PopLDdecay.stat.gz East
 
 
-PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop east.pop -OutStat east.PopLDdecay
-PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop west.pop -OutStat west.PopLDdecay
+Plot_MultiPop.pl -inList K2_multi.list --output K2_West&Central-East.PopLDdecay -bin1 10 -bin2 100
+Plot_MultiPop.pl -inList K3_multi.list --output K3_West&Central&East.PopLDdecay -bin1 10 -bin2 100
 
-Plot_OnePop.pl -inFile east.PopLDdecay.stat.gz --output east.PopLDdecay.stat -bin1 10 -bin2 100
-Plot_OnePop.pl -inFile west.PopLDdecay.stat.gz --output west.PopLDdecay.stat -bin1 10 -bin2 100
-
-vi multi.list
-east.PopLDdecay.stat.gz east
-west.PopLDdecay.stat.gz west
-
-Plot_MultiPop.pl -inList multi.list --output east_west.PopLDdecay -bin1 10 -bin2 100
