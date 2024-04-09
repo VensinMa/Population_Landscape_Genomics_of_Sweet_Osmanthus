2f9b8246aae394ee2b59c447c3b3182528d233a5
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-04-09 18:28:59 +0800

    Update faststructure.sh

--- RE/faststructure.sh
+++ RE/faststructure.sh
@@ -1,1 +1,3 @@
-seq 2 30 | parallel -j 30 "structure.py -K {} --input=/public1/guop/mawx/workspace/186sample/186_filtered.LD.pruned.4DTV.noContig --output=/public1/guop/mawx/workspace/186sample/fastastructure/fast_result/4DTV_faststructure_{} --cv=5 --prior=logistic --seed=123 > faststructure.{}.log 2>&1" &
+cd  /public1/guop/mawx/workspace/186sample/fastastructure
+mkdir fastastructure2_20_result
+seq 2 20 | parallel -j 20 "structure.py -K {} --input=/public1/guop/mawx/workspace/186sample/186_filtered.LD.pruned.noContig --output=/public1/guop/mawx/workspace/186sample/fastastructure/fastastructure2_20_result/LD_faststructure_K_{} --cv=5 --prior=simple --seed=123 > LD_faststructure_K_{}.log 2>&1" &
