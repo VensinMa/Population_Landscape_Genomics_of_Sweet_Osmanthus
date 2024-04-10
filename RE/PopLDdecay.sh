2eaec3ee3fb8d175cee5546e14ff6075f0ef2f13
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-04-10 12:08:15 +0800

    Update PopLDdecay.sh

--- RE/PopLDdecay.sh
+++ RE/PopLDdecay.sh
@@ -3,7 +3,7 @@ cd  /root/workspace/186sample/PopLDdecay
 PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop East.pop -OutStat East.PopLDdecay &
 PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop West.pop -OutStat West.PopLDdecay &
 PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop Central.pop -OutStat Central.PopLDdecay &
-PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop Central-East.pop -OutStat Central-East.PopLDdecay &
+PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -SubPop Central-East.pop -OutStat CentralEast.PopLDdecay &
 
 Plot_OnePop.pl -inFile East.PopLDdecay.stat.gz --output East.PopLDdecay.stat -bin1 10 -bin2 100
 Plot_OnePop.pl -inFile West.PopLDdecay.stat.gz --output West.PopLDdecay.stat -bin1 10 -bin2 100
@@ -11,7 +11,7 @@ Plot_OnePop.pl -inFile Central.PopLDdecay.stat.gz --output Central.PopLDdecay.st
 
 vi K2_multi.list
 West.PopLDdecay.stat.gz West
-Central-East.PopLDdecay.stat.gz Central-East
+CentralEast.PopLDdecay.stat.gz CentralEast
 
 vi K3_multi.list
 West.PopLDdecay.stat.gz West
@@ -19,6 +19,6 @@ Central.PopLDdecay.stat.gz Central
 East.PopLDdecay.stat.gz East
 
 
-Plot_MultiPop.pl -inList K2_multi.list --output K2_West&Central-East.PopLDdecay -bin1 10 -bin2 100
-Plot_MultiPop.pl -inList K3_multi.list --output K3_West&Central&East.PopLDdecay -bin1 10 -bin2 100
+Plot_MultiPop.pl -inList K2_multi.list --output K2_West2CentralEast.PopLDdecay -bin1 10 -bin2 100
+Plot_MultiPop.pl -inList K3_multi.list --output K3_West2Central2East.PopLDdecay -bin1 10 -bin2 100
 
