74bfba93479f58fd4454626f204c9ee7358a995c
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-06-13 19:14:06 +0800

    Update stacks.sh

--- RE/stacks.sh
+++ RE/stacks.sh
@@ -17,5 +17,7 @@ populations -V 186_filtered.LD.pruned.noContig.recode.vcf -M 186sample_32pop.txt
 cd  /public1/guop/mawx/workspace/186sample
 mkdir populations_k2_output
 populations -V 186_filtered.LD.pruned.noContig.recode.vcf -M 186sample_K2.txt  --smooth -t 40  -O ./populations_k2_output
-
-
+mkdir populations_k3_output
+populations -V 186_filtered.LD.pruned.noContig.recode.vcf -M 186sample_K3.txt  --smooth -t 40  -O ./populations_k3_output
+mkdir populations_k1_output
+populations -V 186_filtered.LD.pruned.noContig.recode.vcf -M 186sample_K1.txt  --smooth -t 40  -O ./populations_k1_output
