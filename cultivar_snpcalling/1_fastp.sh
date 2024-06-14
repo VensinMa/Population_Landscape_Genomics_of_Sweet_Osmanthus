b0cfc80af5c1aa5516c6cbc0e4fab457b26aa3ff
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-06-14 19:00:41 +0800

    Update 1_fastp.sh

--- cultivar_snpcalling/1_fastp.sh
+++ cultivar_snpcalling/1_fastp.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # 设置原始数据目录
-raw_data_dir="/public1/guop/mawx/workspace/wild_snpcalling/1.rawdata"
+raw_data_dir="/public1/guop/mawx/workspace/cultivar_snpcalling/fastq.gz/raw_data"
 
 # 设置输出目录
-output_dir="/public1/guop/mawx/workspace/wild_snpcalling/2.cleaned_data"  # 确保这是您的目标输出目录
+clean_data_dir="/public1/guop/mawx/workspace/wild_snpcalling/2.cleaned_data"  # 确保这是您的目标输出目录
 
 # 设置质量报告输出目录
-report_dir="$output_dir/reports"
+report_dir="$clean_data_dir/reports"
 
 # 检查并创建输出目录
-mkdir -p "$output_dir" || { echo "Failed to create output directory: $output_dir"; exit 1; }
+mkdir -p "$clean_data_dir" || { echo "Failed to create output directory: $clean_data_dir"; exit 1; }
 
 # 检查并创建质量报告的输出目录
 mkdir -p "$report_dir" || { echo "Failed to create report directory: $report_dir"; exit 1; }
 
 # 日志文件
-log_file="$output_dir/fastp_processing.log"
+log_file="$clean_data_dir/fastp_processing.log"
 
 # 记录开始时间
 echo "Starting fastp processing at $(date)" | tee -a "$log_file"
@@ -32,7 +32,7 @@ process_sample() {
     number=$(echo "$filename" | awk -F '_' '{print $2}')
 
     # 构建fastp命令
-    fastp_cmd="fastp -w 4 -i $fq1 -I $fq2 -o $output_dir/${prefix}_${number}_1.clean.fq.gz -O $output_dir/${prefix}_${number}_2.clean.fq.gz --html $report_dir/${prefix}_${number}_fastp_report.html --json $report_dir/${prefix}_${number}_fastp_report.json"
+    fastp_cmd="fastp -w 4 -i $fq1 -I $fq2 -o $clean_data_dir/${prefix}_${number}_1.clean.fq.gz -O $clean_data_dir/${prefix}_${number}_2.clean.fq.gz --html $report_dir/${prefix}_${number}_fastp_report.html --json $report_dir/${prefix}_${number}_fastp_report.json"
 
     # 使用后台进程运行fastp命令，并行处理
     echo "Processing $filename at $(date)" | tee -a "$log_file"
