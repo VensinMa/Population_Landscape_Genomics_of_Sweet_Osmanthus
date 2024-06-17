9412fe766d9fdb3096112dc9a01f9ad481dde6b8
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-06-17 11:59:04 +0800

    Update prepare.sh

--- cultivar_snpcalling/prepare.sh
+++ cultivar_snpcalling/prepare.sh
@@ -1,6 +1,5 @@
 
 mkdir /public1/guop/mawx/workspace/cultivar_snpcalling/fastq.gz/raw_data
-
 find /public1/guop/mawx/workspace/cultivar_snpcalling/fastq.gz/ -type f -name "*.fq.gz" -exec cp {} /public1/guop/mawx/workspace/cultivar_snpcalling/fastq.gz/raw_data \;
 
-find /public1/guop/mawx/workspace/cultivar_snpcalling/fastq.gz/ -type f -name "*.fq.gz" -exec mv {} /public1/guop/mawx/workspace/cultivar_snpcalling/fastq.gz/raw_data \;
+
