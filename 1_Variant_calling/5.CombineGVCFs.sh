d44f91b0fdb63d796e22542e004de31065c06b48
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-07-10 18:12:58 +0800

    Update 5.CombineGVCFs.sh

--- 1_Variant_calling/5.CombineGVCFs.sh
+++ 1_Variant_calling/5.CombineGVCFs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 设置输入raw_gvcf文件目录
-raw_gvcf_dir="/public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/raw_gvcf"
+raw_gvcf_dir="/public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/raw_gvcf2"
 
 # 定义参考基因组文件
 reference_genome="/public1/guop/mawx/workspace/wild_snpcalling/0.genome/LYG.hic.fasta"
@@ -16,10 +16,10 @@ combined_gvcf_path="$gatk_dir/combined_raw.gvcf"
 log_file="$gatk_dir/gatk_CombineGVCFs_processing.log"
 
 # 创建输出目录（如果不存在）
-mkdir -p "$(dirname "$combined_gvcf_path")"
+mkdir -p "$(dirname "$combined_gvcf_path")"  "$gatk_dir/tmp"
 
 # 记录脚本开始时间
-echo "CombineGVCFs Script started at $(date)" >> "$log_file"
+echo "CombineGVCFs Script started at $(date)" | tee -a "$log_file"
 
 # 获取所有GVCF文件路径
 gvcf_files=$(find "$raw_gvcf_dir" -name '*.gvcf' | sort)
@@ -31,12 +31,13 @@ for gvcf in $gvcf_files; do
 done
 
 # 运行GATK CombineGVCFs
-echo "Running GATK CombineGVCFs at $(date)" >> "$log_file"
+echo "Running GATK CombineGVCFs at $(date)" | tee -a "$log_file"
 gatk --java-options "-Xms200G -Xmx200G -XX:ParallelGCThreads=20 -Djava.io.tmpdir=${gatk_dir}/tmp" CombineGVCFs \
     -R "$reference_genome" \
     "${variant_args[@]}" \
-    -O "$combined_gvcf_path"
-echo "GATK CombineGVCFs completed at $(date)" >> "$log_file"
+    -O "$combined_gvcf_path"  2>&1 | tee -a "$log_file"
+echo "GATK CombineGVCFs completed at $(date)" | tee -a "$log_file"
 
 # 记录脚本完成时间
-echo "CombineGVCFs Script completed at $(date)" >> "$log_file"
+echo "CombineGVCFs Script completed at $(date)" | tee -a "$log_file"
+
