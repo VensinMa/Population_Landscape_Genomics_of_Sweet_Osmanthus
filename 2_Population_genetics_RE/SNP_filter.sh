d0a4d3cdbfe3a252a4b57417d56ed79ba5261556
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-09-16 17:57:50 +0800

    Update SNP_filter.sh

--- 2_Population_genetics_RE/SNP_filter.sh
+++ 2_Population_genetics_RE/SNP_filter.sh
@@ -1,1 +1,4 @@
+## 剔除多余的网脉木犀 最后保留了6个outgroup个体 O-WMMX O-DSMX O-MXL O-MZGH O-XYWJM O-ZNMX 和 194个内类群个体
 vcftools --vcf all.filtered.snp.unanchor.vcf --keep keep_200sample.id --recode --recode-INFO-all --out 200samples_filtered.snp.unanchor
+
+bcftools reheader -s 200sample_rename.id  200samples_filtered.snp.unanchor.recode.vcf -o 200samples_filtered_renamed.snp.unanchor.recode.vcf 
