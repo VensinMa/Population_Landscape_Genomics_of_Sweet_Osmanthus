c0b21c8109cd7bd12726fbb582512ce981b68f46
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-09-28 14:56:37 +0800

    Update 200sample_NJtree.sh

--- 2_Population_genetics_RE/200sample_NJtree.sh
+++ 2_Population_genetics_RE/200sample_NJtree.sh
@@ -3,7 +3,7 @@
 cd 
 python /root/software/vcf2phylip-2.8/vcf2phylip.py --input 200samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.recode.vcf --fasta --output-prefix 200samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned
 python /root/software/vcf2phylip-2.8/vcf2phylip.py --input 200samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.nomissing.recode.vcf --fasta --output-prefix 200samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.nomissing
-
+python /root/software/vcf2phylip-2.8/vcf2phylip.py --input 195samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.nomissing.final.recode.vcf --fasta --output-prefix 195samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.nomissing.final
 
 
 
@@ -13,6 +13,9 @@ sed 's/:\([0-9.]\+\)\[&&NHX:B=\([0-9]\+\)\]/\2:\1/' 200samples_filtered_renamed.
 
 treebest nj -b 1000 200samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.min4.fasta > 200samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.treebest.out
 
+
+treebest nj -b 1000 195samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.nomissing.final.min4.fasta > 195samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.nomissing.final.treebest.out
+sed 's/:\([0-9.]\+\)\[&&NHX:B=\([0-9]\+\)\]/\2:\1/' 195samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.nomissing.final.treebest.out | awk '{printf $0}' > 195samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.nomissing.final.treebest.nwk
 
 
 
