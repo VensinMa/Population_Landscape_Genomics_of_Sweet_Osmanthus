557016b53e74a4db95679da9e209cc226053a6dc
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-10-17 17:13:25 +0800

    Update and rename NJtree.sh to NJtree_keep_outgroup.sh

--- 2_Population_genetics_RE/NJtree_keep_outgroup.sh
+++ 2_Population_genetics_RE/NJtree_keep_outgroup.sh
@@ -20,9 +20,35 @@ vcftools --vcf /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_v
 vcftools --vcf /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/200samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.recode.vcf \
     --recode --recode-INFO-all  --remove /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/removed_5_outgroup_keep_WMMX.id \
     --out /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/NJtree/195samples_filtered.LD.pruned.removed_5_outgroup_keep_WMMX &
-
+    
+#######################      VCF2Dis 计算距离矩阵构树   ##################################
 cd /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/NJtree
 VCF2Dis -i  195samples_filtered.LD.pruned.removed_5_outgroup_keep_MZGH.recode.vcf  -o 195samples_filtered.LD.pruned.removed_5_outgroup_keep_MZGH_dis.mat    -Rand 0.25 &
 VCF2Dis -i  195samples_filtered.LD.pruned.removed_5_outgroup_keep_DSMX.recode.vcf  -o 195samples_filtered.LD.pruned.removed_5_outgroup_keep_DSMX_dis.mat    -Rand 0.25 &
 VCF2Dis -i  195samples_filtered.LD.pruned.removed_5_outgroup_keep_WMMX.recode.vcf  -o 195samples_filtered.LD.pruned.removed_5_outgroup_keep_WMMX_dis.mat    -Rand 0.25 &
 VCF2Dis -i  195samples_filtered.LD.pruned.removed_5_outgroup_keep_ZNMX.recode.vcf  -o 195samples_filtered.LD.pruned.removed_5_outgroup_keep_ZNMX_dis.mat    -Rand 0.25 &
+
+VCF2Dis -i  /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/200samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.recode.vcf \
+    -o /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/NJtree/200samples_filtered.LD.pruned_dis.mat  \
+    -Rand 0.25 &
+
+
+#######################     treebest  构树   ###################################
+cd /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/NJtree
+python vcf2phylip.py --input 195samples_filtered.LD.pruned.removed_5_outgroup_keep_MZGH.recode.vcf --fasta --output-prefix 195samples_filtered.LD.pruned.removed_5_outgroup_keep_MZGH
+python vcf2phylip.py --input 195samples_filtered.LD.pruned.removed_5_outgroup_keep_DSMX.recode.vcf --fasta --output-prefix 195samples_filtered.LD.pruned.removed_5_outgroup_keep_DSMX
+python vcf2phylip.py --input 195samples_filtered.LD.pruned.removed_5_outgroup_keep_WMMX.recode.vcf --fasta --output-prefix 195samples_filtered.LD.pruned.removed_5_outgroup_keep_WMMX
+python vcf2phylip.py --input 195samples_filtered.LD.pruned.removed_5_outgroup_keep_ZNMX.recode.vcf --fasta --output-prefix 195samples_filtered.LD.pruned.removed_5_outgroup_keep_ZNMX
+python vcf2phylip.py --input /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/200samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned.recode.vcf \
+    --fasta --output-prefix /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/NJtree/200samples_filtered_renamed.snp.unanchor.final.vcftools.LD.pruned
+
+
+
+
+
+
+
+
+
+
+
