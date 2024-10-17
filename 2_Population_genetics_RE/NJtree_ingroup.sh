bf4e36a873ac35f718a1af86b10dd6411e60ba5d
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-10-17 17:47:13 +0800

    Update NJtree_ingroup.sh

--- 2_Population_genetics_RE/NJtree_ingroup.sh
+++ 2_Population_genetics_RE/NJtree_ingroup.sh
@@ -1,1 +1,11 @@
+cd mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/194sample/  && mkdir NJtree
+cp 194samples_snp.nounanchor.renamed.filtered.vcftools.LD.pruned.recode.vcf ./NJtree
+cp 194samples_snp.nounanchor.renamed.filtered.vcftools.LD.pruned.log ./NJtree
+
+
+
+cd mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/194sample/NJtree
+python /public1/guop/mawx/software/vcf2phylip-2.8/vcf2phylip.py --input 194samples_snp.nounanchor.renamed.filtered.vcftools.LD.pruned.recode.vcf --fasta --output-prefix 194samples_filtered.LD.pruned
+treebest nj -b 1000 194samples_filtered.LD.pruned.min4.fasta > 194samples_filtered.LD.pruned.treebest.out
+sed 's/:\([0-9.]\+\)\[&&NHX:B=\([0-9]\+\)\]/\2:\1/' 194samples_filtered.LD.pruned.treebest.out | awk '{printf $0}' > 194samples_filtered.LD.pruned.treebest.nwk
 
