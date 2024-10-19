c96f2f6d676243698f554723385b13702e85e352
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-10-19 19:55:05 +0800

    Update population_structure.sh

--- 2_Population_genetics_RE/population_structure.sh
+++ 2_Population_genetics_RE/population_structure.sh
@@ -7,7 +7,7 @@ plink --vcf /home/vensin/workspace/population_structure/194samples_snp.nounancho
     --make-bed   --out /home/vensin/workspace/population_structure/194samples_filtered.LD.pruned  --keep-allele-order  --allow-extra-chr  
 
 cd /home/vensin/workspace/population_structure/fastastructure/result/
-seq 2 20 | parallel -j 20 "structure.py -K {} \
+seq 2 20 | parallel -j 8 "structure.py -K {} \
     --input=/home/vensin/workspace/population_structure/194samples_filtered.LD.pruned  \
     --output=/home/vensin/workspace/population_structure/fastastructure/result/LD_faststructure_K \
     --cv=5 --prior=simple --seed=123 > LD_faststructure_K_{}.log 2>&1" &
