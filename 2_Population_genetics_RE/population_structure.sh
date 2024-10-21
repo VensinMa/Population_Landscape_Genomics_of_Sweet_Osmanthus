0626a8628556532f5072d8c8ede0141b06058d52
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-10-21 09:31:12 +0800

    Update population_structure.sh

--- 2_Population_genetics_RE/population_structure.sh
+++ 2_Population_genetics_RE/population_structure.sh
@@ -1,15 +1,15 @@
 ###  1、fastastructure
 
-mkdir -p /home/vensin/workspace/population_structure/fastastructure/result
+mkdir -p /home/vensin/workspace/population_structure/faststructure/result
 cd /home/vensin/workspace/population_structure
 ## vcf转bed格式
 plink --vcf /home/vensin/workspace/population_structure/194samples_snp.nounanchor.renamed.filtered.vcftools.LD.pruned.recode.vcf \
     --make-bed   --out /home/vensin/workspace/population_structure/194samples_filtered.LD.pruned  --keep-allele-order  --allow-extra-chr  
 
-cd /home/vensin/workspace/population_structure/fastastructure/result/
+cd /home/vensin/workspace/population_structure/faststructure/result/
 seq 2 20 | parallel -j 8 "structure.py -K {} \
     --input=/home/vensin/workspace/population_structure/194samples_filtered.LD.pruned  \
-    --output=/home/vensin/workspace/population_structure/fastastructure/result/LD_faststructure_K \
+    --output=/home/vensin/workspace/population_structure/faststructure/result/LD_faststructure_K \
     --cv=5 --prior=simple --seed=123 > LD_faststructure_K_{}.log 2>&1" &
 
 
