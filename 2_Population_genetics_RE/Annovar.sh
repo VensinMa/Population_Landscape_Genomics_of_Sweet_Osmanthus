3e31cd558211d6fb66233c0e0ba7395c6267a0d6
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-10-21 11:45:25 +0800

    Create Annovar.sh

---
+++ 2_Population_genetics_RE/Annovar.sh
@@ -0,0 +1,58 @@
+##  annovar官方网站的的下载需要使用邮箱注册后才可下载
+# http://download.openbioinformatics.org/annovar_download_form.php
+
+################################# annovar 安装 ##############################################
+cd /root/workspace/annovar
+wget http://www.openbioinformatics.org/annovar/download/0wgxR2rIVP/annovar.latest.tar.gz
+tar -zxvf annovar.latest.tar.gz
+
+## 安装注释文件格式转换工具 gffread gff3ToGenePred gtfToGenePred
+conda install -c bioconda gffread
+
+conda create -n ucsc
+conda activate ucsc
+conda install bioconda::ucsc-gff3togenepred
+conda install bioconda::ucsc-gtftogenepred
+
+## 添加环境变量 export PATH
+vi ~/.bashrc	
+export PATH=/root/workspace/annovar/annovar:$PATH 
+export PATH=~/anaconda2/envs/ucsc/bin/:$PATH
+source ~/.bashrc
+
+########################  gff格式转gtf格式  ############################################################################
+gffread  /root/workspace/genome/guihua.genomic.gff3 -T -o  guihua.genomic.gtf
+
+gtfToGenePred  -genePredExt guihua.genomic.gtf guihua.genomic_refGene.txt
+
+# gff3ToGenePred  Of.genome.gff3  Of.genome_refGene.txt
+# gff3文件开头必须是##gff-version 3
+
+retrieve_seq_from_fasta.pl --format refGene --seqfile /root/workspace/genome/Osmanthus.genomic.fasta  guihua.genomic_refGene.txt --out guihua.genomic_refGeneMrna.fa
+
+#############################  生成表格格式输入文件  ###################################################################
+convert2annovar.pl -format vcf4 -allsample -withfreq  /root/workspace/186sample/186_filtered_vcftools.noContig.recode.vcf > 186_filtered_vcftools.noContig.annovar.input
+
+## 进行变异注释 (如果需要所有信息，添加-separate) -separate 将每种类型的变异分开注释到不同的文件中
+annotate_variation.pl -geneanno --neargene 2000 -buildver  guihua.genomic -dbtype refGene -outfile AllSNP.annovar -exonsort 186_filtered_vcftools.noContig.annovar.input  ./
+
+##  NOTICE: Finished reading 12551354 lines from VCF file
+##  NOTICE: A total of 12551267 locus in VCF file passed QC threshold, representing 12551267 SNPs (9218405 transitions and 3332862 transversions) and 0 indels/substitutions
+##  NOTICE: Finished writing allele frequencies based on 2334535662 SNP genotypes (1714623330 transitions and 619912332 transversions) and 0 indels/substitutions for 186 samples
+
+####################################   统计每种类型（突变位置）SNP的数量  ###############################################
+cat AllSNP.annovar.variant_function | cut -f 1 | sed 's/;/\n/g' | sort | uniq -c
+1253953 downstream
+ 592470 exonic
+7548615 intergenic
+1906150 intronic
+   3022 splicing
+1398247 upstream
+
+#######################################   统计外显子区域不同突变类型SNP的数量  #############################################
+cat AllSNP.annovar.exonic_variant_function | awk '{print $2}' | sort | uniq -c
+ 322622 nonsynonymous
+   6955 stopgain
+    769 stoploss
+ 261796 synonymous
+    328 unknown
