6b2663391f8f1122c823a92dd4684ef0db4d9483
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-10-21 19:07:21 +0800

    Update Annovar.sh

--- 2_Population_genetics_RE/Annovar.sh
+++ 2_Population_genetics_RE/Annovar.sh
@@ -1,9 +1,10 @@
 ##  annovar官方网站的的下载需要使用邮箱注册后才可下载
-# http://download.openbioinformatics.org/annovar_download_form.php
+# https://www.openbioinformatics.org/annovar/annovar_download_form.php
 
 ################################# annovar 安装 ##############################################
-cd /root/workspace/annovar
-wget http://www.openbioinformatics.org/annovar/download/0wgxR2rIVP/annovar.latest.tar.gz
+mkdir -p /home/vensin/workspace/Annovar && cd /home/vensin/workspace/Annovar
+wget http://www.openbioinformatics.org/annovar/download/0wgxR2rIVP/annovar.latest.tar.gz   # wget https://mawenxin.cn/annovar.latest.tar.gz
+
 tar -zxvf annovar.latest.tar.gz
 
 ## 安装注释文件格式转换工具 gffread gff3ToGenePred gtfToGenePred
@@ -15,26 +16,27 @@ conda install bioconda::ucsc-gff3togenepred
 conda install bioconda::ucsc-gtftogenepred
 
 ## 添加环境变量 export PATH
-vi ~/.bashrc	
-export PATH=/root/workspace/annovar/annovar:$PATH 
-export PATH=~/anaconda2/envs/ucsc/bin/:$PATH
+echo 'export PATH=/home/vensin/workspace/Annovar/annovar:$PATH' >> ~/.bashrc
+echo 'export PATH=/home/vensin/anaconda3/envs/ucsc/bin/:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
 ########################  gff格式转gtf格式  ############################################################################
-gffread  /root/workspace/genome/guihua.genomic.gff3 -T -o  guihua.genomic.gtf
+gffread  /home/vensin/workspace/Annovar/final.gff -T -o LYG.hic.gtf
 
-gtfToGenePred  -genePredExt guihua.genomic.gtf guihua.genomic_refGene.txt
+gtfToGenePred  -genePredExt LYG.hic.gtf LYG.hic_refGene.txt
 
 # gff3ToGenePred  Of.genome.gff3  Of.genome_refGene.txt
 # gff3文件开头必须是##gff-version 3
 
-retrieve_seq_from_fasta.pl --format refGene --seqfile /root/workspace/genome/Osmanthus.genomic.fasta  guihua.genomic_refGene.txt --out guihua.genomic_refGeneMrna.fa
+retrieve_seq_from_fasta.pl --format refGene --seqfile /home/vensin/workspace/Annovar/LYG.hic.fasta  LYG.hic_refGene.txt --out LYG.hic_refGeneMrna.fa
 
 #############################  生成表格格式输入文件  ###################################################################
-convert2annovar.pl -format vcf4 -allsample -withfreq  /root/workspace/186sample/186_filtered_vcftools.noContig.recode.vcf > 186_filtered_vcftools.noContig.annovar.input
+convert2annovar.pl -format vcf4 -allsample -withfreq \
+  /home/vensin/workspace/population_structure/194samples_snp.nounanchor.renamed.filtered.vcftools.LD.pruned.recode.vcf \
+  > 194samples_filtered.LD.pruned.annovar.input
 
 ## 进行变异注释 (如果需要所有信息，添加-separate) -separate 将每种类型的变异分开注释到不同的文件中
-annotate_variation.pl -geneanno --neargene 2000 -buildver  guihua.genomic -dbtype refGene -outfile AllSNP.annovar -exonsort 186_filtered_vcftools.noContig.annovar.input  ./
+annotate_variation.pl -geneanno --neargene 2000 -buildver  LYG.hic -dbtype refGene -outfile LYG.hic.snp.annovar -exonsort 194samples_filtered.LD.pruned.annovar.input  ./
 
 ##  NOTICE: Finished reading 12551354 lines from VCF file
 ##  NOTICE: A total of 12551267 locus in VCF file passed QC threshold, representing 12551267 SNPs (9218405 transitions and 3332862 transversions) and 0 indels/substitutions
