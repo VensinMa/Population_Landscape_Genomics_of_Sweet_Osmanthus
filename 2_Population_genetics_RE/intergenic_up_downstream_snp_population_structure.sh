bffcc6267c60a4876af3dcec2443bb50f68c9e44
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-10-24 00:03:42 +0800

    Update intergenic_up_downstream_snp_population_structure.sh

--- 2_Population_genetics_RE/intergenic_up_downstream_snp_population_structure.sh
+++ 2_Population_genetics_RE/intergenic_up_downstream_snp_population_structure.sh
@@ -10,12 +10,16 @@ vcftools --vcf 194samples_snp.nounanchor.renamed.filtered.vcftools.recode.vcf \
     --positions intergenic_up_downstream.snp.id \
     --recode --recode-INFO-all \
     --out 194samples_filtered.intergenic_up_downstream.snp
+##  After filtering, kept 8073005 out of a possible 11452745 Sites
+##  Run Time = 2179.00 seconds
 
 plink --vcf 194samples_filtered.intergenic_up_downstream.snp.recode.vcf \
     --indep-pairwise 50 5 0.2 \
     --out LD  \
     --allow-extra-chr  \
     --set-missing-var-ids @:# 
+## Pruning complete.  6958009 of 8073005 variants removed.
+## 1114996
 
 sed 's/:/ /g' LD.prune.in > intergenic_up_downstream.LD.prune.in.snp.id
 
@@ -23,6 +27,8 @@ vcftools --vcf 194samples_snp.nounanchor.renamed.filtered.vcftools.recode.vcf \
     --positions intergenic_up_downstream.LD.prune.in.snp.id \
     --recode --recode-INFO-all \
     --out 194samples_filtered.intergenic_up_downstream.LD.prune
+##  After filtering, kept 1114996 out of a possible 11452745 Sites
+##  Run Time = 335.00 seconds
 
 mkdir -p /home/vensin/workspace/intergenic_up_downstream_faststructure/result
 cd /home/vensin/workspace/intergenic_up_downstream_faststructure
@@ -35,7 +41,7 @@ plink --vcf /home/vensin/workspace/Annovar/194samples_filtered.intergenic_up_dow
 cd /home/vensin/workspace/intergenic_up_downstream_faststructure/result/
 seq 2 10 | parallel -j 10 "structure.py -K {} \
     --input=/home/vensin/workspace/intergenic_up_downstream_faststructure/194samples_filtered.intergenic_up_downstream.LD.prune  \
-    --output=/home/vensin/workspace/population_structure/faststructure/result/LD_faststructure_K \
+    --output=/home/vensin/workspace/population_structure/faststructure/result/intergenic_up_downstream_LD_faststructure_K \
     --cv=5 --prior=simple --seed=123 > intergenic_up_downstream_LD_faststructure_K_{}.log 2>&1" &
 
 chooseK.py --input intergenic_up_downstream_LD_faststructure_K
