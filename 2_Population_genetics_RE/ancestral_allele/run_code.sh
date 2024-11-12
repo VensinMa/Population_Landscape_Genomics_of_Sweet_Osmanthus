f580f7f3e5027a6a83788f63df929f2786493d20
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-11-12 18:02:29 +0800

    Update run_code.sh

--- 2_Population_genetics_RE/ancestral_allele/run_code.sh
+++ 2_Population_genetics_RE/ancestral_allele/run_code.sh
@@ -1,3 +1,44 @@
+### 学院服务器上
+#  /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/200samples_filtered_renamed.snp.unanchor.final.gatk.vcf
+
+cd  /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools
+
+###  剔除外类群样本   O_DSMX  O_MXL   O_XYWJM   保留浙南木犀 网脉木犀 蒙自桂花 O_ZNMX  NFM_1   O_MZGH
+vcftools --vcf /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/200samples_filtered_renamed.snp.unanchor.final.gatk.vcf \
+    --recode --recode-INFO-all  --remove /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/removed_3_outgroup.id \
+    --out /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/197samples_filtered_renamed.snp.unanchor.final.gatk.vcf &
+
+vcftools --vcf 197samples_filtered_renamed.snp.unanchor.final.gatk.vcf \
+    --min-alleles 2 \
+    --max-alleles 2 \
+    --minDP 5 \
+    --minGQ 10 \
+    --minQ 30 \
+    --min-meanDP 6 \
+    --max-missing 1 \
+    --maf 0.05 \
+    --recode \
+    --recode-INFO-all \
+    --out 197samples_filtered.snp.nomissing
+
+###  剔除外类群样本   O_DSMX  O_MXL  O_XYWJM  O_MZGH  保留浙南木犀 网脉木犀  O_ZNMX  NFM_1  
+vcftools --vcf /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/200samples_filtered_renamed.snp.unanchor.final.gatk.vcf \
+    --recode --recode-INFO-all  --remove /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/removed_4_outgroup.id \
+    --out /public1/guop/mawx/workspace/wild_snpcalling/4.gatk_gvcf/merged_vcf/vcftools/196samples_filtered_renamed.snp.unanchor.final.gatk.vcf &
+
+vcftools --vcf 196samples_filtered_renamed.snp.unanchor.final.gatk.vcf \
+    --min-alleles 2 \
+    --max-alleles 2 \
+    --minDP 5 \
+    --minGQ 10 \
+    --minQ 30 \
+    --min-meanDP 6 \
+    --max-missing 1 \
+    --maf 0.05 \
+    --recode \
+    --recode-INFO-all \
+    --out 196samples_filtered.snp.nomissing
+
 # 1、将vcf文件转换为est-sfs软件所需的输入文件格式
 cd /home/vensin/workspace/est-sfs/prepare_est-sfs
 python vcf_to_estsfs.py  194samples_filtered.intergenic_up_downstream.LD.prune.nomissing.plink.vcf  DRS-7 LCJ-7
