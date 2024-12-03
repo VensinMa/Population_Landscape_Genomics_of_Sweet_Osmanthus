c9d8c1b66a4760890dce2d2803d37693e392cfdc
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2024-12-03 20:53:30 +0800

    Update combine_gene_annotations.py

--- 2_Population_genetics_RE/combine_gene_annotations.py
+++ 2_Population_genetics_RE/combine_gene_annotations.py
@@ -3,8 +3,8 @@
 import csv
 
 # 读取nr注释和annovar注释文件
-nr_annotations_file = "/home/vensin/workspace/Annovar/LYG.longest.pep.uniq.Nr.modified.annotations"
-annovar_annotations_file = "/home/vensin/workspace/nr.annotations/LYG.hic.snp.annovar.variant_function"
+nr_annotations_file = "LYG.longest.pep.uniq.Nr.modified.annotations/LYG.longest.pep.uniq.Nr.modified.annotations"
+annovar_annotations_file = "/home/vensin/workspace/Annovar/LYG.hic.snp.annovar.variant_function"
 output_file = "/home/vensin/workspace/Annovar/result/merged_output.txt"
 
 # 存储第一个文件数据，按基因ID索引
