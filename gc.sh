b20443a96bd2d9244857f7e8765def618d6b6a5e
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-01-12 13:36:17 +0800

    Create gc.sh

---
+++ gc.sh
@@ -0,0 +1,24 @@
+
+# VCF文件转为BED BED转为RAW RAW转LFMM    LD过滤后 (2768836 variants, 311 people)
+plink --vcf /home/vensin/gc/311_filtered.recode.LD.pruned.recode_noscaffold.vcf\
+      --recode --allow-extra-chr \
+      --out  /home/vensin/gc/311_filtered.recode.LD.pruned.plink
+  
+plink --file /home/vensin/gc/311_filtered.recode.LD.pruned.plink \
+    --recodeA --allow-extra-chr  \
+    --out /home/vensin/gc/311_filtered.recode.LD.pruned.plink.recodeA
+    
+sed '1d; s/NA/9/g' /home/vensin/gc/311_filtered.recode.LD.pruned.plink.recodeA.raw | \
+awk '{ $1=$2=$3=$4=$5=$6=""; print substr($0, index($0,$7)) }' > /home/vensin/gc/311_filtered.recode.LD.pruned.plink.recodeA.lfmm
+
+# VCF文件转为BED BED转为RAW RAW转LFMM    零缺失过滤后 (974035 variants, 311 people)
+plink --vcf /home/vensin/gc/311_filtered_maf0.1_nomissing_noscaffold.vcf\
+      --recode --allow-extra-chr \
+      --out  /home/vensin/gc/311_filtered_maf0.1_nomissing.plink
+  
+plink --file /home/vensin/gc/311_filtered_maf0.1_nomissing.plink \
+    --recodeA --allow-extra-chr  \
+    --out /home/vensin/gc/311_filtered_maf0.1_nomissing.plink.recodeA
+    
+sed '1d; s/NA/9/g' /home/vensin/gc/311_filtered_maf0.1_nomissing.plink.recodeA.raw | \
+awk '{ $1=$2=$3=$4=$5=$6=""; print substr($0, index($0,$7)) }' > /home/vensin/gc/311_filtered_maf0.1_nomissing.plink.recodeA.lfmm
