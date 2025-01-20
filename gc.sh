4621d53747ddf7e41ab39dfd922f82fd8cb3b4b7
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-01-20 18:10:10 +0800

    Update gc.sh

--- gc.sh
+++ gc.sh
@@ -1,4 +1,43 @@
 # VCF文件转为BED BED转为RAW RAW转LFMM    0.01缺失过滤后 (6071370 variants, 311 people)
+plink --vcf /home/vensin/gc/311_filtered_miss1_noscaffold.recode.vcf\
+      --recode --allow-extra-chr \
+      --out  /home/vensin/gc/311_filtered_miss1_noscaffold.plink
+  
+plink --file /home/vensin/gc/311_filtered_miss1_noscaffold.plink \
+    --recodeA --allow-extra-chr  \
+    --out /home/vensin/gc/311_filtered_miss1_noscaffold.plink.recodeA
+    
+sed '1d; s/NA/9/g' /home/vensin/gc/311_filtered_miss1_noscaffold.plink.recodeA.raw | \
+awk '{ $1=$2=$3=$4=$5=$6=""; print substr($0, index($0,$7)) }' > /home/vensin/gc/311_filtered_miss1_noscaffold.plink.recodeA.lfmm
+
+awk '{print "Chr"$1":"$4}' 311_filtered_miss1_noscaffold.plink.map > 311_filtered_miss1_noscaffold.plink.ID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# VCF文件转为BED BED转为RAW RAW转LFMM    0.01缺失过滤后 (6071370 variants, 311 people)
 plink --vcf /home/vensin/gc/311_filtered_maf0.1_miss0.99.recode_noscaffold.vcf\
       --recode --allow-extra-chr \
       --out  /home/vensin/gc/311_filtered_maf0.1_miss0.99.plink
