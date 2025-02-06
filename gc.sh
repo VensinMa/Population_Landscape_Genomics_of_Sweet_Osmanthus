50d32ee4b9b17b6f9da6b281436c0b5acbfafcd2
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-02-06 12:39:59 +0800

    Update gc.sh

--- gc.sh
+++ gc.sh
@@ -1,16 +1,16 @@
 # VCF文件转为BED BED转为RAW RAW转LFMM    0.01缺失过滤后 (6071370 variants, 311 people)
-plink --vcf /home/vensin/gc/311_filtered_miss1_noscaffold.recode.vcf\
+plink --vcf /home/vensin/gc/311_filtered.recode_noscaffold.miss0.99.recode.vcf\
       --recode --allow-extra-chr \
-      --out  /home/vensin/gc/311_filtered_miss1_noscaffold.plink
+      --out  /home/vensin/gc/311_filtered.recode_noscaffold.miss0.99.plink
   
-plink --file /home/vensin/gc/311_filtered_miss1_noscaffold.plink \
+plink --file /home/vensin/gc/311_filtered.recode_noscaffold.miss0.99.plink \
     --recodeA --allow-extra-chr  \
-    --out /home/vensin/gc/311_filtered_miss1_noscaffold.plink.recodeA
+    --out /home/vensin/gc/311_filtered.recode_noscaffold.miss0.99.plink.recodeA
     
-sed '1d; s/NA/9/g' /home/vensin/gc/311_filtered_miss1_noscaffold.plink.recodeA.raw | \
-awk '{ $1=$2=$3=$4=$5=$6=""; print substr($0, index($0,$7)) }' > /home/vensin/gc/311_filtered_miss1_noscaffold.plink.recodeA.lfmm
+sed '1d; s/NA/9/g' /home/vensin/gc/311_filtered.recode_noscaffold.miss0.99.plink.recodeA.raw | \
+awk '{ $1=$2=$3=$4=$5=$6=""; print substr($0, index($0,$7)) }' > /home/vensin/gc/311_filtered.recode_noscaffold.miss0.99.plink.recodeA.lfmm
 
-awk '{print "Chr"$1":"$4}' 311_filtered_miss1_noscaffold.plink.map > 311_filtered_miss1_noscaffold.plink.ID
+awk '{print "Chr"$1":"$4}' 311_filtered.recode_noscaffold.miss0.99.plink.map > 311_filtered.recode_noscaffold.miss0.99.plink.ID
 
 
 
