f0cf1ee0bfb7ed7360a8065b9092a93b945e5b17
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-02-06 13:41:07 +0800

    Update gc.sh

--- gc.sh
+++ gc.sh
@@ -1,4 +1,4 @@
-# VCF文件转为BED BED转为RAW RAW转LFMM    0.01缺失过滤后 (6071370 variants, 311 people)
+# VCF文件转为BED BED转为RAW RAW转LFMM    0.01缺失过滤后 (8648736 variants, 311 people)
 plink --vcf /home/vensin/gc/311_filtered.recode_noscaffold.miss0.99.recode.vcf\
       --recode --allow-extra-chr \
       --out  /home/vensin/gc/311_filtered.recode_noscaffold.miss0.99.plink
@@ -9,7 +9,6 @@ plink --file /home/vensin/gc/311_filtered.recode_noscaffold.miss0.99.plink \
     
 sed '1d; s/NA/9/g' /home/vensin/gc/311_filtered.recode_noscaffold.miss0.99.plink.recodeA.raw | \
 awk '{ $1=$2=$3=$4=$5=$6=""; print substr($0, index($0,$7)) }' > /home/vensin/gc/311_filtered.recode_noscaffold.miss0.99.plink.recodeA.lfmm
-
 awk '{print "Chr"$1":"$4}' 311_filtered.recode_noscaffold.miss0.99.plink.map > 311_filtered.recode_noscaffold.miss0.99.plink.ID
 
 
@@ -17,61 +16,3 @@ awk '{print "Chr"$1":"$4}' 311_filtered.recode_noscaffold.miss0.99.plink.map > 3
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# VCF文件转为BED BED转为RAW RAW转LFMM    0.01缺失过滤后 (6071370 variants, 311 people)
-plink --vcf /home/vensin/gc/311_filtered_maf0.1_miss0.99.recode_noscaffold.vcf\
-      --recode --allow-extra-chr \
-      --out  /home/vensin/gc/311_filtered_maf0.1_miss0.99.plink
-  
-plink --file /home/vensin/gc/311_filtered_maf0.1_miss0.99.plink \
-    --recodeA --allow-extra-chr  \
-    --out /home/vensin/gc/311_filtered_maf0.1_miss0.99.plink.recodeA
-    
-sed '1d; s/NA/9/g' /home/vensin/gc/311_filtered_maf0.1_miss0.99.plink.recodeA.raw | \
-awk '{ $1=$2=$3=$4=$5=$6=""; print substr($0, index($0,$7)) }' > /home/vensin/gc/311_filtered_maf0.1_miss0.99.plink.recodeA.lfmm
-
-
-
-
-# VCF文件转为BED BED转为RAW RAW转LFMM    LD过滤后 (2768836 variants, 311 people)
-plink --vcf /home/vensin/gc/311_filtered.recode.LD.pruned.recode_noscaffold.vcf\
-      --recode --allow-extra-chr \
-      --out  /home/vensin/gc/311_filtered.recode.LD.pruned.plink
-  
-plink --file /home/vensin/gc/311_filtered.recode.LD.pruned.plink \
-    --recodeA --allow-extra-chr  \
-    --out /home/vensin/gc/311_filtered.recode.LD.pruned.plink.recodeA
-    
-sed '1d; s/NA/9/g' /home/vensin/gc/311_filtered.recode.LD.pruned.plink.recodeA.raw | \
-awk '{ $1=$2=$3=$4=$5=$6=""; print substr($0, index($0,$7)) }' > /home/vensin/gc/311_filtered.recode.LD.pruned.plink.recodeA.lfmm
-
-# VCF文件转为BED BED转为RAW RAW转LFMM    零缺失过滤后 (974035 variants, 311 people)
-plink --vcf /home/vensin/gc/311_filtered_maf0.1_nomissing_noscaffold.vcf\
-      --recode --allow-extra-chr \
-      --out  /home/vensin/gc/311_filtered_maf0.1_nomissing.plink
-  
-plink --file /home/vensin/gc/311_filtered_maf0.1_nomissing.plink \
-    --recodeA --allow-extra-chr  \
-    --out /home/vensin/gc/311_filtered_maf0.1_nomissing.plink.recodeA
-    
-sed '1d; s/NA/9/g' /home/vensin/gc/311_filtered_maf0.1_nomissing.plink.recodeA.raw | \
-awk '{ $1=$2=$3=$4=$5=$6=""; print substr($0, index($0,$7)) }' > /home/vensin/gc/311_filtered_maf0.1_nomissing.plink.recodeA.lfmm
