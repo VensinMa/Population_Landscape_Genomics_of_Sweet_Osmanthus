758b6ba0d25ab90f62b2973eb15c0d5ffdd525d9
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-03-16 20:26:21 +0800

    Update PopLDdecay.sh

--- 2_Population_genetics_RE/PopLDdecay.sh
+++ 2_Population_genetics_RE/PopLDdecay.sh
@@ -1,24 +1,47 @@
+conda create -n poplddecay
+conda activate poplddecay
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+conda install -c bioconda -c conda-forge poplddecay
+echo 'export PATH="~/anaconda3/envs/poplddecay/bin:$PATH"' >> ~/.bashrc
+
+
+
 cd  /root/workspace/186sample/PopLDdecay
 
-PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -MaxDist 10 -SubPop East.pop -OutStat East.PopLDdecay_10K &
-PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -MaxDist 10 -SubPop West.pop -OutStat West.PopLDdecay_10K &
-PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -MaxDist 10 -SubPop Central.pop -OutStat Central.PopLDdecay_10K &
-PopLDdecay  -InVCF /mnt/e/mwx/workspace/pop/186_filtered_vcftools.noContig.recode.vcf -MaxDist 10 -SubPop Central-East.pop -OutStat CentralEast.PopLDdecay_10K &
+### Step1: 计算 LD decay
+PopLDdecay  -InVCF /home/vensin/workspace/PopLDdecay/194_vcftools.recode.vcf -MaxDist 50 -SubPop East.pop -OutStat East.PopLDdecay
+PopLDdecay  -InVCF /home/vensin/workspace/PopLDdecay/194_vcftools.recode.vcf -MaxDist 50 -SubPop Central.pop -OutStat Central.PopLDdecay
+PopLDdecay  -InVCF /home/vensin/workspace/PopLDdecay/194_vcftools.recode.vcf -MaxDist 50 -SubPop West-GZ.pop -OutStat West-GZ.PopLDdecay
+PopLDdecay  -InVCF /home/vensin/workspace/PopLDdecay/194_vcftools.recode.vcf -MaxDist 50 -SubPop West-YN.pop -OutStat West-YN.PopLDdecay
 
-Plot_OnePop.pl -inFile East.PopLDdecay_10K.stat.gz --output East.PopLDdecay_10K.stat -bin1 10 -bin2 100
-Plot_OnePop.pl -inFile West.PopLDdecay_10K.stat.gz --output West.PopLDdecay_10K.stat -bin1 10 -bin2 100
-Plot_OnePop.pl -inFile Central.PopLDdecay_10K.stat.gz --output Central.PopLDdecay_10K.stat -bin1 10 -bin2 100
+PopLDdecay  -InVCF /home/vensin/workspace/PopLDdecay/194_vcftools.recode.vcf -MaxDist 50 -SubPop Central-East.pop -OutStat Central-East.PopLDdecay
+PopLDdecay  -InVCF /home/vensin/workspace/PopLDdecay/194_vcftools.recode.vcf -MaxDist 50 -SubPop West.pop -OutStat West.PopLDdecay
 
-vi K2_multi_10K.list
-West.PopLDdecay_10K.stat.gz West
-CentralEast.PopLDdecay_10K.stat.gz CentralEast
+### Step2: 绘图
+## 单个谱系绘图
+Plot_OnePop.pl -inFile East.PopLDdecay.stat.gz --output East.PopLDdecay.stat -bin1 10 -bin2 100
+Plot_OnePop.pl -inFile Central.PopLDdecay.stat.gz --output Central.PopLDdecay.stat -bin1 10 -bin2 100
+Plot_OnePop.pl -inFile West-GZ.PopLDdecay.stat.gz --output West-GZ.PopLDdecay.stat -bin1 10 -bin2 100
+Plot_OnePop.pl -inFile West-YN.PopLDdecay.stat.gz --output West-YN.PopLDdecay.stat -bin1 10 -bin2 100
 
-vi K3_multi_10K.list
-West.PopLDdecay_10K.stat.gz West
-Central.PopLDdecay_10K.stat.gz Central
-East.PopLDdecay_10K.stat.gz East
+Plot_OnePop.pl -inFile Central-East.PopLDdecay.stat.gz --output Central-East.PopLDdecay.stat -bin1 10 -bin2 100
+Plot_OnePop.pl -inFile West.PopLDdecay.stat.gz --output West.PopLDdecay.stat -bin1 10 -bin2 100
 
+## 多谱系绘图
+cat <<EOF > K4_multi.list
+East.PopLDdecay.stat.gz East
+Central.PopLDdecay.stat.gz Central
+West-GZ.PopLDdecay.stat.gz West-GZ
+West-YN.PopLDdecay.stat.gz West-YN
+EOF
 
-Plot_MultiPop.pl -inList K2_multi_10K.list --output K2_West2CentralEast.PopLDdecay_10K -bin1 10 -bin2 100
-Plot_MultiPop.pl -inList K3_multi_10K.list --output K3_West2Central2East.PopLDdecay_10K -bin1 10 -bin2 100
+cat <<EOF > K2_multi.list
+Central-East.stat Central-East
+West.PopLDdecay.stat West
+EOF
 
+Plot_MultiPop.pl -inList K4_multi.list --output K4.PopLDdecay -bin1 10 -bin2 100
+Plot_MultiPop.pl -inList K2_multi.list --output K2.PopLDdecay -bin1 10 -bin2 100
+
+gzip -d -k Central-East.PopLDdecay.stat.bin.gz Central.PopLDdecay.stat.bin.gz  East.PopLDdecay.stat.bin.gz  West-GZ.PopLDdecay.stat.bin.gz  West-YN.PopLDdecay.stat.bin.gz  West.PopLDdecay.stat.bin.gz
